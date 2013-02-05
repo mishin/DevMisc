1960526176e7cf9f2703224bc2ef5aab75779314
James Manning <james.manning@gmail.com>
2013-02-05 10:56:59 -0500

    script to regenerate pdf files via powerpoint

---
+++ scripts/office/powerpoint-regenerate-pdf.ps1
@@ -0,0 +1,34 @@
+﻿param (
+    #[string[]] $pptxPaths = $(throw 'must specify paths to powerpoint files')
+    [string[]] $pptxPaths = "Z:\Marketing\Clients\San Francisco Toyota\February 2013\1 - San Jose Metro\Toyota Sunnyvale\February 2013-Toyota Sunnyvale-Assessment.pptx"
+)
+
+Set-StrictMode -Version Latest
+
+Add-type -AssemblyName office
+$application = New-Object -ComObject powerpoint.application
+#$application.visible = [Microsoft.Office.Core.MsoTriState]::msoTrue
+
+foreach ($pptxPath in $pptxPaths)
+{
+    $pdfPath = [System.IO.Path]::ChangeExtension($pptxPath, '.pdf')
+
+    Write-Debug "Opening $pptxPath"
+    # http://msdn.microsoft.com/en-us/library/office/microsoft.office.interop.powerpoint.presentations.open(v=office.14).aspx
+    $presentation = $application.Presentations.open(
+        $pptxPath, 
+        [Microsoft.Office.Core.MsoTriState]::msoTrue,     # readonly   == true
+        [Microsoft.Office.Core.MsoTriState]::msoFalse,    # untitled   == false
+        [Microsoft.Office.Core.MsoTriState]::msoFalse)    # WithWindow == false
+    Write-Debug "Saving $pdfPath"
+    $presentation.SaveCopyAs($pdfPath, [Microsoft.Office.Interop.PowerPoint.PpSaveAsFileType]::ppSaveAsPDF);
+
+    Write-Debug "Closing $pptxPath"
+    $presentation.Close()
+}
+
+$application.Quit()
+
+# likely not needed, but keeping in case it helps the RCW/COM clean up
+[gc]::collect()
+[gc]::WaitForPendingFinalizers()
