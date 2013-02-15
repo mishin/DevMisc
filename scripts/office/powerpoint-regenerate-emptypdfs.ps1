4a14aec09e6d38fabb6b8d8784a75b1e3918f388
James Manning <james.manning@gmail.com>
2013-02-15 14:31:05 -0500

    script to regenerate all reports with empty pdf files

---
+++ scripts/office/powerpoint-regenerate-emptypdfs.ps1
@@ -0,0 +1,15 @@
+﻿param (
+    [string] $targetDirectory = "Z:\Marketing\Clients\Gulf States Toyota\February 2013"
+)
+
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+while ($true)
+{
+    $zeroBytePdfs = dir -r $targetDirectory -fi *.pdf | ?{ $_.Length -eq 0 }
+    if (-not $zeroBytePdfs) { return; }
+
+    $pptxPaths = $zeroBytePdfs | %{ [io.path]::ChangeExtension($_.FullName, '.pptx') }
+
+    & "$scriptPath\powerpoint-regenerate-pdf.ps1" $pptxPaths
+}
