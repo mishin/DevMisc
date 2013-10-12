5af0d25efe5e7f0541a721d7dd92444b78f69255
James Manning <james.manning@gmail.com>
2013-10-12 19:01:57 -0400

    add some boxstarter / psget stuff to setup.ps1

--- env-setup/setup.ps1
+++ env-setup/setup.ps1
@@ -14,11 +14,78 @@
 # first set execution policy so we can run scripts
 Set-ExecutionPolicy Unrestricted -Force
 
-# install psget
-(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
-
 # install chocolatey
 iex ((new-object net.webclient).DownloadString("http://bit.ly/psChocInstall"))
+
+
+############################
+##### Boxstarter setup #####
+############################
+cinstm boxstarter
+
+Get-Module Boxstarter.* -ListAvailable | Import-Module
+
+Install-WindowsUpdate -AcceptEula
+#Update-ExecutionPolicy Unrestricted
+#Move-LibraryDirectory "Personal" "$env:UserProfile\skydrive\documents"
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+Set-TaskbarSmall
+Enable-RemoteDesktop
+
+#cinstm VisualStudioExpress2012Web
+cinstm fiddler
+#cinstm mssqlserver2012express
+cinstm git-credential-winstore
+#cinstm console-devel
+#cinstm skydrive
+#cinstm poshgit
+cinstm windbg
+
+cinst Microsoft-Hyper-V-All -source windowsFeatures
+cinst IIS-WebServerRole -source windowsfeatures
+cinst IIS-HttpCompressionDynamic -source windowsfeatures
+cinst IIS-ManagementScriptingTools -source windowsfeatures
+cinst IIS-WindowsAuthentication -source windowsfeatures
+#cinst TelnetClient -source windowsFeatures
+
+#Install-ChocolateyPinnedTaskBarItem "$env:windir\system32\mstsc.exe"
+#Install-ChocolateyPinnedTaskBarItem "$env:programfiles\console\console.exe"
+
+#copy-item (Join-Path (Get-PackageRoot($MyInvocation)) 'console.xml') -Force $env:appdata\console\console.xml
+
+#Install-ChocolateyVsixPackage xunit http://visualstudiogallery.msdn.microsoft.com/463c5987-f82b-46c8-a97e-b1cde42b9099/file/66837/1/xunit.runner.visualstudio.vsix
+#Install-ChocolateyVsixPackage autowrocktestable http://visualstudiogallery.msdn.microsoft.com/ea3a37c9-1c76-4628-803e-b10a109e7943/file/73131/1/AutoWrockTestable.vsix
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # don't install these packages, use the ones that come with GitHub for Windows
 #cinstm git
@@ -88,6 +155,9 @@ $linqpadXmlSettings = @"
 [io.file]::writealltext("$linqpadAppDataDirectory\PluginLocations.txt", "$linqpadScriptDirectory\lib")
 [io.file]::writealltext("$linqpadAppDataDirectory\RoamingUserOptions.xml", $linqpadXmlSettings)
 
+
+# install psget
+(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 
 Install-Module Find-String
 Install-Module ImageSorter
